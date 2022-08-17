25b6332159606c6ffb1020cfcbb585b13fe97070
hukim <hukim@artiasolution.com>
2022-08-18 05:38:50 +0900

    fix argmax -> max to calculate top_1_probability

--- experiment/extract_embeddings.py
+++ experiment/extract_embeddings.py
@@ -67,40 +67,47 @@ class Embedder():
     def extract_embeddings(self, label):
         if self.model is None:
             raise ValueError("You need to call get_model() to load your model first.")
-
         self.model.eval()
         files = self.get_class_files(label)
         embeddings = []
-        top_1 = []
+        top_1_class = []
+        top_1_prob = []
         file_paths = []
         for file in files:
             image, class_id = self.get_img(file)
             with torch.no_grad():
                 embedding = self.model.embedding(self.model.patch_embedding(image.unsqueeze(0).to(self.device)))
                 score = self.model.fc(embedding)
+                probs = torch.softmax(score, dim=-1)
             embeddings.append(embedding[0])
-            top_1.append(torch.argmax(score[0]).item())
+            top_1_class.append(torch.argmax(score[0]).item())
+            top_1_prob.append(torch.max(probs[0]).item())
             file_paths.append(file)
         embeddings = torch.stack(embeddings)
-        top_1 = torch.tensor(top_1)
-        return embeddings, top_1, top_1 == class_id, file_paths
+        top_1_class = torch.tensor(top_1_class)
+        top_1_prob = torch.tensor(top_1_prob)
+        correctness = top_1_class == class_id
+        return embeddings, top_1_class, top_1_prob, correctness, file_paths
 
     def save_embeddings(self, path, label):
-        embeddings, top_1, correctness, file_paths = self.extract_embeddings(label)
+        embeddings, top_1_class, top_1_prob, correctness, file_paths = self.extract_embeddings(label)
         os.makedirs(join(path, "embeddings"), exist_ok=True)
         os.makedirs(join(path, "correctness"), exist_ok=True)
         os.makedirs(join(path, "filename"), exist_ok=True)
+        os.makedirs(join(path, "top_1_probability"), exist_ok=True)
         np.save(join(path, "embeddings", label+".npy"), embeddings.cpu().numpy())
+        np.save(join(path, "top_1_probability", label+".npy"), top_1_prob.cpu().numpy())
         np.save(join(path, "correctness", label+".npy"), correctness.cpu().numpy())
         with open(join(path, "filename", label), "wb") as fp:
             pickle.dump(file_paths, fp)
 
     def load_embeddings(self, path, label):
         embeddings = np.load(join(path, "embeddings", label+".npy"))
+        top_1_prob = np.load(join(path, "top_1_probability", label+".npy"))
         correctness = np.load(join(path, "correctness", label+".npy"))
         with open(join(path, "filename", label), "rb") as fp:
             file_paths = pickle.load(fp)
-        return embeddings, correctness, file_paths
+        return embeddings, top_1_prob, correctness, file_paths
 
 '''
 import torch
