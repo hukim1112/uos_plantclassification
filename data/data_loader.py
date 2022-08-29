eabc57491b896f927cab615f90f3d7244e0bdfe1
yjchoi <ccyjun123@naver.com>
2022-08-29 15:13:39 +0900

    add dataset and dataloader class for training 148 target categories

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,6 +1,5 @@
 from config.path import PATH
-from .PlantNet import PlantNet300K
-from torchvision import transforms
+from .PlantNet import PlantNet300K, PlantNetTarget148
 from torch.utils.data import DataLoader
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
@@ -30,6 +29,19 @@ def get_plantnet(transforms=None, batch_size=32):
     data_loaders = {}
     for split in splits:
         dataset = PlantNet300K(root=PATH["PLANTNET-300K"], split=split, shuffle=(split=="train"), transform=transforms[split])
+        if split == "train":
+            class_to_name = dataset.class_to_name
+        data_loaders[split] = DataLoader(dataset, batch_size=batch_size, shuffle=(split=="train"), num_workers=4)
+    return data_loaders, class_to_name
+
+def get_plantnet_for148(transforms=None, batch_size=32):
+    if transforms is None:
+        transforms = default_transforms
+        
+    splits = ["train", "val", "test"]
+    data_loaders = {}
+    for split in splits:
+        dataset = PlantNetTarget148(root=PATH["PLANTNET-300K"], split=split, shuffle=(split=="train"), transform=transforms[split])
         if split == "train":
             class_to_name = dataset.class_to_name
         data_loaders[split] = DataLoader(dataset, batch_size=batch_size, shuffle=(split=="train"), num_workers=4)
