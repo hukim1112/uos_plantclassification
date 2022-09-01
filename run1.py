13d6fafc68ea2f74971b143c5af58a03a9279e3d
hukim <hukim@artiasolution.com>
2022-09-01 15:12:27 +0900

    remove run1 and run2

--- run1.py
+++
@@ -1,7 +0,0 @@
-from experiment.run_efficientB4_mini_plantnet import *
-
-name="exp1"
-exp_dir = f"/home/files/experiments/mini_plantnet/efficientB4/{name}"
-weight_dir = f"{exp_dir}/checkpoints/checkpoint.pt"
-log_dir = f"{exp_dir}/logs/"
-exp_set1(weight_dir, log_dir)
